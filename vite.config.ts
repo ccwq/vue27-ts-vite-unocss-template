409e50d342864af4cc3f645136f47468326f034b
ccwq <scwqd@qq.com>
2022-09-01 17:12:40 +0800

    chore: 增加less支持

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { extractorSplit } from '@unocss/core'
 import {rules, extractorPugFactory} from "luss/dist/unocss-luss.js"
 import {presetUno} from "unocss";
 
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,5 +23,29 @@ export default defineConfig({
                 extractorSplit,
             ],
         }),
-    ]
+    ],
+    resolve:{
+        alias:{
+            "src": path.join(__dirname, "src"),
+        }
+    },
+    css:{
+        preprocessorOptions: {
+
+            // https://lesscss.org/usage/#less-options
+            less: {
+                javascriptEnabled: true,
+
+                modifyVars: {
+
+                    // 追加变量声明到 每个文件的末尾,覆盖变量
+                    // "@formLabelColor":"#f00",
+
+                    // 自动导入变量生命文件
+                    hack: `true; @import (reference) "${path.resolve(__dirname, 'src/styles/config.less')}";`,
+                },
+
+            }
+        }
+    }
 })
