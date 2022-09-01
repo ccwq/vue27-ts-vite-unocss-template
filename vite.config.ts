c14bbf509f1dad905c690078b925b2156e63d03f
ccwq <scwqd@qq.com>
2022-09-01 15:05:22 +0800

    chore: unocss对pug内class的支持

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 
+import extractorPug from '@unocss/extractor-pug'
+import { extractorSplit } from '@unocss/core'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        Unocss({ /* options */ }),
+        Unocss({
+            extractors: [
+                extractorPug(),
+                extractorSplit,
+            ],
+        }),
 
     ]
 })
