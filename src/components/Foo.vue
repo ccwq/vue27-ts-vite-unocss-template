4c65d9a6856517e9c01f45b311a4a872fb6ef9de
ccwq <scwqd@qq.com>
2022-09-01 17:13:18 +0800

    feat: less目录结构初始化
    使用方式请查看Foo.vue

--- src/components/Foo.vue
+++ src/components/Foo.vue
@@ -1,7 +1,14 @@
-<script lang="ts">
-
-
+<script setup>
+import {ref} from 'vue'
+const message = ref('Foo:Hello World')
 </script>
 <template lang="pug">
-.div.bg-green.mt10 你好foo
+.div.bg-green.mt10 {{message}}
 </template>
+
+<style lang="less">
+@import "src/styles/mixins.less";
+.div{
+    border: 5px solid @formLabelColor;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 import App from './App.vue'
 
 import "uno.css"
+import "luss/dist/lite.css"
 
 createApp(App).mount('#app')
